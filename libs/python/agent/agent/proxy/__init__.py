82bb9eacbe065e41eb7fd303917d757544539bee
Dillon DuPont <ddupont@mit.edu>
2025-09-02 16:03:51 -0400

    Removed unused folder

--- libs/python/agent/agent/proxy/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Proxy module for exposing ComputerAgent over HTTP and P2P connections.
-"""
-
-from .server import ProxyServer
-from .handlers import ResponsesHandler
-
-__all__ = ['ProxyServer', 'ResponsesHandler']
