8ef5b22dce0a842dd9c32632fae35b3f669557a2
James Murdza <james@jamesmurdza.com>
2025-10-24 17:25:44 -0700

    Install dev dependencies before running type checker

--- scripts/typescript-typecheck.js
+++ scripts/typescript-typecheck.js
@@ -2,6 +2,7 @@
 const { execSync } = require('child_process');
 
 try {
+  execSync('pnpm -C libs/typescript install', { stdio: 'inherit' });
   execSync('pnpm -C libs/typescript -r run typecheck', { stdio: 'inherit' });
 } catch (err) {
   process.exit(1);
