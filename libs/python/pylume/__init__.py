88930810ac7c41af65f4760e0c526ad3edde9538
James Murdza <james@jamesmurdza.com>
2025-10-28 16:56:03 -0700

    Remove PyLume library

--- libs/python/pylume/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-PyLume Python SDK - A client library for managing macOS VMs with PyLume.
-"""
-
-from pylume.exceptions import *
-from pylume.models import *
-from pylume.pylume import *
-
-__version__ = "0.1.0"
